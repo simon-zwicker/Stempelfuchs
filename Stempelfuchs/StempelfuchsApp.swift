899b2892d31e8c92d6814c2a80d7be8cf33427b4
Simon Zwicker <simon.zwicker@dak.de>
2025-05-02 15:14:46 +0200

    Initial Commit

---
+++ Stempelfuchs/StempelfuchsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StempelfuchsApp.swift
+//  Stempelfuchs
+//
+//  Created by Simon Zwicker on 02.05.25.
+//
+
+import SwiftUI
+
+@main
+struct StempelfuchsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StempelfuchsTests/StempelfuchsTests.swift
@@ -0,0 +1,17 @@
+//
+//  StempelfuchsTests.swift
+//  StempelfuchsTests
+//
+//  Created by Simon Zwicker on 02.05.25.
+//
+
+import Testing
+@testable import Stempelfuchs
+
+struct StempelfuchsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
